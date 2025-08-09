1a3ccc5524ec6c8819c7e57354d4fe0f1899f25b
Russell SB <r.sammutbonnici@gmail.com>
2025-08-09 16:20:03 +0100

    Experimenting with second order derivative savgol filter. Improvement on moving average for dealing with seasonality

--- poc_gradual.py
+++ poc_gradual.py
@@ -5,23 +5,22 @@ import pandas as pd
 df = pd.read_csv('./data/series_gradual.csv', infer_datetime_format=True, index_col="date")
 df.plot(figsize=(20,3))
 
-# %%
-df['value'].values
-
-# %%
-df['diff'] = df['value'].diff(periods=1)
-df.plot(figsize=(20,3))
-
-# %%
-df['rolling_avg'] = df['value'].rolling(14).mean()
-df.plot(figsize=(20,3))
 
 
 # %%
 import matplotlib.pyplot as plt
-df['diff_2'] = (df['rolling_avg'].diff(periods=1) * 20) - 14
-ax = df.plot(figsize=(20,3))
+df['rolling_avg'] = df['value'].rolling(14, center=True).mean()
+df['diff_2'] = (df['rolling_avg'].diff(periods=1) * 20) 
+ax = df[['diff_2', 'value']].plot(figsize=(20,3))
 ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
 plt.show()
 
 # %%
+from scipy.signal import savgol_filter
+import matplotlib.pyplot as plt
+df['sg_filter'] = savgol_filter(df['value'], window_length=14, polyorder=1, deriv=1)
+df['sg_filter'] = df['sg_filter'].shift(7)
+df['diff_2'] = (df['sg_filter'].diff(periods=1)) 
+ax = df[['value', 'sg_filter']].plot(figsize=(20,3), secondary_y='sg_filter')
+ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+plt.show()
