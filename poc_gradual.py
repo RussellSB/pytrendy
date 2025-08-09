92fd23d2ab34e7d9a7331815feb546cd883216f1
Russell SB <r.sammutbonnici@gmail.com>
2025-08-09 15:31:09 +0100

    Experimenting with differencing, rolling average, & displacement

--- poc_gradual.py
+++ poc_gradual.py
@@ -6,3 +6,22 @@ df = pd.read_csv('./data/series_gradual.csv', infer_datetime_format=True, index_
 df.plot(figsize=(20,3))
 
 # %%
+df['value'].values
+
+# %%
+df['diff'] = df['value'].diff(periods=1)
+df.plot(figsize=(20,3))
+
+# %%
+df['rolling_avg'] = df['value'].rolling(14).mean()
+df.plot(figsize=(20,3))
+
+
+# %%
+import matplotlib.pyplot as plt
+df['diff_2'] = (df['rolling_avg'].diff(periods=1) * 20) - 14
+ax = df.plot(figsize=(20,3))
+ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+plt.show()
+
+# %%
