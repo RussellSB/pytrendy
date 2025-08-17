2daef091e5ebde408917da7bb12e01933c8e43f1
Russell SB <r.sammutbonnici@gmail.com>
2025-08-17 12:07:33 +0100

    Added a todo for testing

---
+++ tests/todo.py
@@ -0,0 +1,6 @@
+# Test on Gradual - Initial
+# Test on Gradual - with diff magitudes
+# Test on Gradual - with ever increasing amount of noise
+# Test on Refine - make sure exansion works well
+# Test on Abrupt/Refine - make sure contraction works well
+
