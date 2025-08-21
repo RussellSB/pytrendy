f2c6c711213be420203d01b5fdb58626eb217e45
Russell SB <r.sammutbonnici@gmail.com>
2025-08-21 21:20:11 +0100

    Solved abrupt case, and adjusting with neighbour shifting

--- pytrendy/main.py
+++ pytrendy/main.py
@@ -1,9 +1,9 @@
-# # %%
-# %load_ext autoreload
-# %autoreload 2
+# %%
+%load_ext autoreload
+%autoreload 2
 
-# # %%
-# %reload_ext autoreload
+# %%
+%reload_ext autoreload
 
 #%%
 import pandas as pd
@@ -20,16 +20,33 @@ def main(df:pd.DataFrame, date_col:str, value_col: str):
     df.set_index(date_col, inplace=True)
     df = process_signals(df, value_col)
     segments = get_segments(df)
-    segments = refine_segments(df, value_col, segments)
-    # segments = analyse_segments(df, value_col, segments)
+    segments = refine_segments(df, value_col, segments) #TODO: abrupt contractor (3), and grouping (1?)
+    segments = analyse_segments(df, value_col, segments)
     plot_pytrendy(df, value_col, segments)
 
     return segments
 
-# Use Case 1: Simple
-df = pd.read_csv('./data/series_gradual.csv')
+# Use Case 2: Abrupt
+df = pd.read_csv('./data/series_abrupt.csv')
 segments = main(df, date_col='date', value_col='value')
 # segments
 
 # %%
+# Use Case 1: Simple
+# df = pd.read_csv('./data/series_gradual.csv')
+# segments = main(df, date_col='date', value_col='value')
+
+# %%
+
+
+# %%
 segments
+
+#%%
+# import numpy as np
+# for noise_std in [0, 2, 5, 10, 20, 50]:
+#     print(f'Noise value: {noise_std}')
+#     df = pd.read_csv('./data/series_gradual.csv')
+#     df['value_noisy'] = df['value'] + np.random.normal(0, noise_std, size=len(df))
+#     segments = main(df, date_col='date', value_col='value_noisy')
+# %%
--- pytrendy/segments_refine.py
+++ pytrendy/segments_refine.py
@@ -1,14 +1,36 @@
 import pandas as pd
 from copy import deepcopy
+import numpy as np
 
-def refine_segments(df: pd.DataFrame, value_col: str, segments: list):
+THRESHOLD_DISTANCE = 3
+
+def _update_prev_segment(i, new_start, segments, segments_refined):
+    """Shift previous segment end if overlapping with updated start (or original start)."""
+    if i == 0:
+        return
+    distance_refined = (pd.to_datetime(new_start) - pd.to_datetime(segments_refined[i - 1]['end'])).days
+    distance_orig = (pd.to_datetime(segments[i]['start']) - pd.to_datetime(segments[i - 1]['end'])).days
+    if distance_refined <= THRESHOLD_DISTANCE or distance_orig <= THRESHOLD_DISTANCE:
+        segments_refined[i - 1]['end'] = (pd.to_datetime(new_start) - pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+
+
+def _update_next_segment(i, new_end, segments, segments_refined):
+    """Shift next segment start if overlapping with updated end (or original end)."""
+    if i == len(segments_refined) - 1:
+        return
+    distance_refined = (pd.to_datetime(segments_refined[i + 1]['start']) - pd.to_datetime(new_end)).days
+    distance_orig = (pd.to_datetime(segments[i + 1]['start']) - pd.to_datetime(segments[i]['end'])).days
+    if distance_refined <= THRESHOLD_DISTANCE or distance_orig <= THRESHOLD_DISTANCE:
+        segments_refined[i + 1]['start'] = (pd.to_datetime(new_end) + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+
+
+def expand_contract_segments(df: pd.DataFrame, value_col: str, segments: list):
     """
-    Post-process detected segments by refining their start and end points.
+    Post-process detected segments by assessing their start and end points.
     Adjusts boundaries by looking ±7 days around each boundary for more precision.
     Is there an appropriately higher or lower point worth taking? Take it.
+    If it increases the segment - "expand". If it decreases the segment - "contract".
     """
-    THRESHOLD_DISTANCE = 3
-
     segments_refined = deepcopy(segments)
 
     def _get_window_df(center, days=7):
@@ -17,25 +39,8 @@ def refine_segments(df: pd.DataFrame, value_col: str, segments: list):
         post = (pd.to_datetime(center) + pd.Timedelta(days=days)).strftime('%Y-%m-%d')
         return df.loc[pre:post].copy()
 
-    def _update_prev_segment(i, new_start):
-        """Shift previous segment end if overlapping with updated start (or original start)."""
-        if i == 0:
-            return
-        distance_refined = (pd.to_datetime(new_start) - pd.to_datetime(segments_refined[i - 1]['end'])).days
-        distance_orig = (pd.to_datetime(segments[i]['start']) - pd.to_datetime(segments[i - 1]['end'])).days
-        if distance_refined <= THRESHOLD_DISTANCE or distance_orig <= THRESHOLD_DISTANCE:
-            segments_refined[i - 1]['end'] = (pd.to_datetime(new_start) - pd.Timedelta(days=1)).strftime('%Y-%m-%d')
-
-    def _update_next_segment(i, new_end):
-        """Shift next segment start if overlapping with updated end (or original end)."""
-        if i == len(segments_refined) - 1:
-            return
-        distance_refined = (pd.to_datetime(segments_refined[i + 1]['start']) - pd.to_datetime(new_end)).days
-        distance_orig = (pd.to_datetime(segments[i + 1]['start']) - pd.to_datetime(segments[i]['end'])).days
-        if distance_refined <= THRESHOLD_DISTANCE or distance_orig <= THRESHOLD_DISTANCE:
-            segments_refined[i + 1]['start'] = (pd.to_datetime(new_end) + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
-
     for i, segment in enumerate(segments_refined):
+
         start_df = _get_window_df(segment['start'])
         end_df = _get_window_df(segment['end'])
 
@@ -48,19 +53,68 @@ def refine_segments(df: pd.DataFrame, value_col: str, segments: list):
         else:
             continue
 
-        print(i, segment['direction'])
         # refine start
         if new_start != pd.to_datetime(segment['start']):
             segments_refined[i]['start'] = new_start.strftime('%Y-%m-%d')
-            _update_prev_segment(i, new_start)
+            _update_prev_segment(i, new_start, segments, segments_refined)
 
         # refine end
         if new_end != pd.to_datetime(segment['end']):
             segments_refined[i]['end'] = new_end.strftime('%Y-%m-%d')
-            _update_next_segment(i, new_end)
+            _update_next_segment(i, new_end, segments, segments_refined)
 
-        print('reasy')
+    return segments_refined
 
-    print(segments[5])
-    print(segments_refined[5])
+
+def shave_abrupt_trends(df: pd.DataFrame, value_col: str, segments: list):
+    """
+    Handles case of abrupt trends since changepoint detection is missed by rolling statistics
+    We analyse the segment for diff outliers, and take the earliest and latest points from here.
+    """
+    import matplotlib.pyplot as plt
+    segments_refined = deepcopy(segments)
+    for i, segment in enumerate(segments_refined):
+
+        if segment['direction'] not in ['Up', 'Down']: 
+            continue
+
+        # Get start end padded for some leniency
+        start = pd.to_datetime(segment['start']) - pd.Timedelta(days=7)
+        end = pd.to_datetime(segment['end']) + pd.Timedelta(days=7)
+        df_segment = df.loc[start:end]
+        df_segment['diff'] = df_segment[value_col].diff()
+        df_segment = df_segment.iloc[1:]
+
+        df_segment['diff']
+        df_segment['z_score'] = (df_segment['diff'] - df_segment['diff'].mean()) / df_segment['diff'].std()
+
+        df_segment['abrupt_flag'] = 0
+        df_segment.loc[df_segment['z_score'].abs() > 2, 'abrupt_flag'] = 1
+
+        # ax = df_segment[[value_col, 'diff']].plot(figsize=(20,3), secondary_y='diff')
+        # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+        # plt.show()
+        # ax = df_segment[[value_col, 'z_score']].plot(figsize=(20,3), secondary_y='z_score')
+        # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+        # plt.show()
+        # ax = df_segment[[value_col, 'abrupt_flag']].plot(figsize=(20,3), secondary_y='abrupt_flag')
+        # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+        # plt.show()
+
+        new_start = df_segment.loc[df_segment['abrupt_flag'] == 1].index[0] - pd.Timedelta(days=1)
+        new_end = df_segment.loc[df_segment['abrupt_flag'] == 1].index[-1]
+
+        segments_refined[i]['start'] = new_start.strftime('%Y-%m-%d')
+        _update_prev_segment(i, new_start, segments, segments_refined)
+        segments_refined[i]['end'] = new_end.strftime('%Y-%m-%d')
+        _update_next_segment(i, new_end, segments, segments_refined)
+
+        # update prev and next appropriately
+    return segments_refined
+
+
+
+def refine_segments(df: pd.DataFrame, value_col: str, segments: list):
+    segments_refined = expand_contract_segments(df, value_col, segments)
+    segments_refined = shave_abrupt_trends(df, value_col, segments)
     return segments_refined
