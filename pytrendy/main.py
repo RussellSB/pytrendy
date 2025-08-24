3664e2ed9f5d16574bc5860bf9939b59078743f5
Russell SB <r.sammutbonnici@gmail.com>
2025-08-24 12:15:47 +0100

    Cleaning up main

--- pytrendy/main.py
+++ pytrendy/main.py
@@ -20,37 +20,30 @@ def main(df:pd.DataFrame, date_col:str, value_col: str):
     df.set_index(date_col, inplace=True)
     df = process_signals(df, value_col)
     segments = get_segments(df)
-    segments = refine_segments(df, value_col, segments) #TODO: grouping (1?)
+    segments = refine_segments(df, value_col, segments)
     segments = analyse_segments(df, value_col, segments)
     plot_pytrendy(df, value_col, segments)
 
     return segments
 
-# segments = main(df, date_col='date', value_col='value_noisy')
-
-# Run
+# Example Runs
 df = pd.read_csv('./data/series_synthetic.csv')
 segments = main(df, date_col='date', value_col='abrupt')
 segments = main(df, date_col='date', value_col='gradual')
+segments = main(df, date_col='date', value_col='gradual-noisy-20')
 
 
 # %%
-segments
+# noise_std = 20
+# df = pd.read_csv('./data/series_synthetic.csv')
+# df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
+# segments = main(df, date_col='date', value_col='value_noisy')
+# segments = main(df, date_col='date', value_col='value_noisy')
 
-# %%
-noise_std = 20
-df = pd.read_csv('./data/series_synthetic.csv')
-df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
-segments = main(df, date_col='date', value_col='value_noisy')
-
-# %%
-import numpy as np
-for noise_std in [0, 10, 20, 50]:
-    print(f'Noise value: {noise_std}')
-    df = pd.read_csv('./data/series_synthetic.csv')
-    df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
-    segments = main(df, date_col='date', value_col='value_noisy')
-
-
-
-# %%
+# # %%
+# import numpy as np
+# for noise_std in [0, 10, 20, 50]:
+#     print(f'Noise value: {noise_std}')
+#     df = pd.read_csv('./data/series_synthetic.csv')
+#     df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
+#     segments = main(df, date_col='date', value_col='value_noisy')
