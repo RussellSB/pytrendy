8e956adf1f0d3956431f1d9b62a879cf4ac38a07
Russell SB <r.sammutbonnici@gmail.com>
2025-08-25 12:20:04 +0100

    Improving user experience. Adding helpers for results object.

---
+++ pytrendy/detect_trends.py
@@ -0,0 +1,99 @@
+import pandas as pd
+from .process_signals import process_signals
+from .segments_get import get_segments
+from .segments_refine import refine_segments
+from .segments_analyse import analyse_segments
+from .plot_pytrendy import plot_pytrendy
+
+class PyTrendyResults: 
+    """Wrapper for segment results."""
+
+    def __init__(self, segments):
+        self.segments = segments
+        self.set_best()
+
+    def set_best(self):
+        """
+        results.best returns best based on total_change (cumulative sum of differences). 
+        This prioritises both longest segment length (days) and steepness if trend.
+        """
+        best = self.segments[0]
+        if best['direction'] not in ['Up', 'Down']:
+            best = None
+        self.best = best
+
+    def filter_segments(self, direction:str='All',  by:str='change_rank'):
+        """
+        Simple helper for getting segments 
+        - filtered by direction ['Any', 'Up/Down', 'Up', 'Down', 'Flat', 'Noise']
+        - sorted by time_index (ascending) or change_rank (descending)
+        """
+        segments = self.segments
+
+        # Sort segments by index/rank
+        if by == 'change_rank':
+            segments = sorted(segments, key=lambda x: abs(x.get('total_change', 0)), reverse=True) # descending
+        elif by == 'time_index':
+            segments = sorted(segments, key=lambda x: abs(x.get('time_index', 0))) # ascending
+        else:
+            print(f'{by} is not a valid sort category. Please try one of [\'time_index\', \'change_rank\']')
+
+        # Filter segments by direction
+        if direction != 'Any':
+            allowed_directions = {'Up', 'Down'} if direction == 'Up/Down' else {direction}
+            segments = [seg for seg in segments if seg['direction'] in allowed_directions]
+        if direction not in ['Any', 'Up/Down', 'Up', 'Down', 'Flat', 'Noise']:
+            print(f'{direction} is not a valid sort category. Please try one of [\'Any\', \'Up/Down\', \'Up\', \'Down\', \'Flat\', \'Noise\']')
+
+        return segments
+    
+
+    def summarize(self):
+        pass
+
+
+def detect_trends(df:pd.DataFrame, date_col:str, value_col: str, plot=True):
+    """
+    Detects trends through a 5-step pipeline.
+    1. Process Signals; Rolling statistics for detecting segments of flat, noise, and uptrend/downtrend
+    2. Get Segments; Partitions the areas into segments provided long enough
+    3. Refine Segments: Post processes the data to cater for rolling statistic edge cases.
+    4. Analyse Segments: Provides change stats and ranking for each trend.
+    5. Plot Trends (optional): Plots results with matplotlib.
+
+    Returns: PyTrendyResults (obj)
+    """
+    df = df.copy()
+    df[date_col] = pd.to_datetime(df[date_col])
+    df.set_index(date_col, inplace=True)
+
+    df = process_signals(df, value_col)
+    segments = get_segments(df)
+    segments = refine_segments(df, value_col, segments)
+    segments = analyse_segments(df, value_col, segments)
+    if plot: plot_pytrendy(df, value_col, segments)
+
+    results = PyTrendyResults(segments)
+    return results
+
+# Example Runs
+df = pd.read_csv('./data/series_synthetic.csv')
+results = detect_trends(df, date_col='date', value_col='abrupt')
+results = detect_trends(df, date_col='date', value_col='gradual')
+results = detect_trends(df, date_col='date', value_col='gradual-noisy-20')
+
+
+# %%
+# noise_std = 20
+# df = pd.read_csv('./data/series_synthetic.csv')
+# df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
+# results = main(df, date_col='date', value_col='value_noisy')
+# results = main(df, date_col='date', value_col='value_noisy')
+
+# # %%
+# import numpy as np
+# for noise_std in [0, 10, 20, 50]:
+#     print(f'Noise value: {noise_std}')
+#     df = pd.read_csv('./data/series_synthetic.csv')
+#     df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
+#     results = main(df, date_col='date', value_col='value_noisy')
