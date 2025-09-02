86fd02a266bc3db1e001735cb0abe0e2f90aaf91
barshadeb <debbarsha1503@gmail.com>
2025-09-02 21:13:13 +0100

    fix(visual): abrupt trend leaving white line when neighbouring

--- tests/test.py
+++ tests/test.py
@@ -7,29 +7,25 @@
 %reload_ext autoreload
 
 #%%
-import pandas as pd
-from detect_trends import detect_trends
-from data_loader import load_data
-
+import pytrendy as pt
 # Example Runs
 
-df = load_data('series_synthetic')
-# results = detect_trends(df, date_col='date', value_col='gradual-noisy-20')
-# results = detect_trends(df, date_col='date', value_col='abrupt')
-results = detect_trends(df, date_col='date', value_col='gradual', plot=False)
-results.filter_segments(sort_by='time_index')[:3]
+df = pt.load_data('series_synthetic')
+results_gradual = pt.detect_trends(df, date_col='date', value_col='gradual', plot=True)
+results_abrupt = pt.detect_trends(df, date_col='date', value_col='abrupt', plot=True)
 
 # %%
 # noise_std = 20
-# df = pd.read_csv('./data/series_synthetic.csv')
+# df = pt.load_data('series_synthetic')
 # df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
-# results = main(df, date_col='date', value_col='value_noisy')
-# results = main(df, date_col='date', value_col='value_noisy')
+# results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
+# results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
 
 # # %%
 # import numpy as np
 # for noise_std in [0, 10, 20, 50]:
 #     print(f'Noise value: {noise_std}')
-#     df = pd.read_csv('./data/series_synthetic.csv')
+#     df = pt.load_data('series_synthetic')
 #     df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
-#     results = main(df, date_col='date', value_col='value_noisy')
+#     results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
+# %%
