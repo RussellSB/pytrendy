23417d34f8eeaa954a52e1e548e842a25c9e097a
barshadeb <debbarsha1503@gmail.com>
2025-09-02 21:13:13 +0100

    fix(visual): abrupt trend leaving white line when neighbouring

--- tests/test.py
+++ tests/test.py
@@ -26,15 +26,16 @@ results = pt.detect_trends(df, date_col='date', value_col='abrupt')
 
 # %%
 # noise_std = 20
-# df = pd.read_csv('./data/series_synthetic.csv')
+# df = pt.load_data('series_synthetic')
 # df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
-# results = main(df, date_col='date', value_col='value_noisy')
-# results = main(df, date_col='date', value_col='value_noisy')
+# results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
+# results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
 
 # # %%
 # import numpy as np
 # for noise_std in [0, 10, 20, 50]:
 #     print(f'Noise value: {noise_std}')
-#     df = pd.read_csv('./data/series_synthetic.csv')
+#     df = pt.load_data('series_synthetic')
 #     df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
-#     results = main(df, date_col='date', value_col='value_noisy')
+#     results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
+# %%
