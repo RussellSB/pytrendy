1e40328f8c49faaeaa9db715d04b9b76a2c887ef
Russell SB <r.sammutbonnici@gmail.com>
2025-09-03 07:33:56 +0100

    chore(zero detection): improving it not just from trailing but also leading

--- pytrendy/process_signals.py
+++ pytrendy/process_signals.py
@@ -30,7 +30,9 @@ def process_signals(df:pd.DataFrame, value_col: str):
     # 3. Noise detection via SNR. 
 
     # 3.1 Compute zero flat edge cases
-    df['zeros_pct'] = (df[value_col] == 0).rolling(WINDOW_NOISE, center=False, min_periods=1).sum() / WINDOW_NOISE
+    df['zeros_pct_trailing'] = (df[value_col] == 0).rolling(WINDOW_NOISE, min_periods=1).sum() / WINDOW_NOISE
+    df['zeros_pct_leading'] = ((df[value_col] == 0).iloc[::-1].rolling(window=WINDOW_NOISE, min_periods=1).sum() / WINDOW_NOISE).iloc[::-1]
+    df['zeros_pct'] = df[['zeros_pct_trailing', 'zeros_pct_leading']].max(axis=1)
 
     # 3.2 Compute the SNR
     df['signal'] = df[value_col].rolling(window=WINDOW_NOISE, center=True, min_periods=1).mean()
@@ -40,6 +42,14 @@ def process_signals(df:pd.DataFrame, value_col: str):
     # 3.3 Define noise flag when SNR & not all zero
     df['noise_flag'] = 0
     df.loc[(df['snr'] <= THRESHOLD_NOISE) & (df['zeros_pct'] <= THRESHOLD_ZEROS), 'noise_flag'] = 1
+    
+    ax = df[[value_col, 'zeros_pct_trailing']].plot(figsize=(20,3), secondary_y='zeros_pct_trailing')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()   
+    
+    ax = df[[value_col, 'zeros_pct_leading']].plot(figsize=(20,3), secondary_y='zeros_pct_leading')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
 
     ax = df[[value_col, 'zeros_pct']].plot(figsize=(20,3), secondary_y='zeros_pct')
     ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
