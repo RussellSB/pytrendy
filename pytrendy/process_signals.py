ebe6578f18fae3dbb7cf9eb798f75e960f824d1b
Russell SB <r.sammutbonnici@gmail.com>
2025-09-04 17:47:31 +0100

    fix(flat detection): improved so not sensitive to mostly 0 signals

--- pytrendy/process_signals.py
+++ pytrendy/process_signals.py
@@ -9,7 +9,6 @@ def process_signals(df:pd.DataFrame, value_col: str):
     WINDOW_FLAT = int(WINDOW_SMOOTH*0.5)
     WINDOW_NOISE = int(WINDOW_SMOOTH*0.5)
 
-    THRESHOLD_FLAT = 0 # Sensitivity to detecting flats (recommended 0-0.5)
     THRESHOLD_ZEROS = 0.50 # Sensitivity to zeros pct, needed for nosie by snr (0-1)
     THRESHOLD_NOISE = 5 # Sensitivity to detecting noise (recommended 0-10)
     THRESHOLD_SMOOTH = 0.25 # Sensetivity to detecting trends (recommended 0-0.5)
@@ -24,8 +23,13 @@ def process_signals(df:pd.DataFrame, value_col: str):
     df['smoothed_std_trailing'] = df['smoothed'].rolling(WINDOW_FLAT).std()
     df['smoothed_std'] = df['smoothed_std'].fillna(df['smoothed_std_leading']).fillna(df['smoothed_std_trailing'])
     df['flat_flag'] = 0
-    threshold_flat = df[value_col].rolling(WINDOW_FLAT, center=True).std().quantile(THRESHOLD_FLAT) # initially set at 2 for series_gradual example
-    df.loc[df['smoothed_std'] <= threshold_flat, 'flat_flag'] = 1 # can comment out to not care about flats. Just take flats with up/down
+    rolling_std = df[value_col].rolling(WINDOW_FLAT, center=True).std()
+    min_nonzero_std = rolling_std[rolling_std > 0].min()
+    df.loc[df['smoothed_std'] <= min_nonzero_std, 'flat_flag'] = 1 # can comment out to not care about flats. Just take flats with up/down
+
+    # ax = df[[value_col, 'flat_flag']].plot(figsize=(20,3), secondary_y='flat_flag')
+    # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    # plt.show()
 
     # 3. Noise detection via SNR. 
 
@@ -42,6 +46,10 @@ def process_signals(df:pd.DataFrame, value_col: str):
     # 3.3 Define noise flag when SNR & not all zero
     df['noise_flag'] = 0
     df.loc[(df['snr'] <= THRESHOLD_NOISE) & (df['zeros_pct'] <= THRESHOLD_ZEROS), 'noise_flag'] = 1
+
+    # ax = df[[value_col, 'noise_flag']].plot(figsize=(20,3), secondary_y='noise_flag')
+    # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    # plt.show()
 
     # 4. Detect up/down trend. Uses first derivates of savgol filter (like diff). 
     # Results in signal that's uptrend > 0, else down. As long as its not on a flat.
