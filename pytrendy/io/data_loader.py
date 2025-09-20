d04c49c715cb93e52d6c43ac912ce531028f7188
Russell SB <r.sammutbonnici@gmail.com>
2025-09-20 10:51:02 +0100

    fix(data loader): making windows compatible, with not just linux specific seperators '/'

--- pytrendy/io/data_loader.py
+++ pytrendy/io/data_loader.py
@@ -8,5 +8,6 @@ def load_data(dataset:str='series_synthetic') -> pd.DataFrame:
         print(f'{dataset} is not a valid dataset to load from Pytrendy. Please try either of {options}')
 
     dir_path = str(Path(__file__).resolve().parent)
-    df = pd.read_csv(dir_path + '/data/' + dataset + '.csv')
+    file_path = dir_path / "data" / f"{dataset}.csv"
+    df = pd.read_csv(file_path)
     return df
