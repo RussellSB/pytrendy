cc24e4d1d4bfa80a413da30467c1a0204bdf1958
Russell SB <r.sammutbonnici@gmail.com>
2025-09-20 10:52:34 +0100

    fix(data loader): Path object instead of str

--- pytrendy/io/data_loader.py
+++ pytrendy/io/data_loader.py
@@ -7,7 +7,7 @@ def load_data(dataset:str='series_synthetic') -> pd.DataFrame:
     if dataset not in options:
         print(f'{dataset} is not a valid dataset to load from Pytrendy. Please try either of {options}')
 
-    dir_path = str(Path(__file__).resolve().parent)
+    dir_path = Path(__file__).resolve().parent
     file_path = dir_path / "data" / f"{dataset}.csv"
     df = pd.read_csv(file_path)
     return df
