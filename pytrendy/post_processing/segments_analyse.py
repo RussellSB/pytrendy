0e4ee1516aa72f0985f04fafe297e36875971909
Russell SB <r.sammutbonnici@gmail.com>
2025-09-21 19:29:01 +0100

    fix(warnings): fixing warnings in analyse segments so doesnt divide by zero

--- pytrendy/post_processing/segments_analyse.py
+++ pytrendy/post_processing/segments_analyse.py
@@ -10,12 +10,18 @@ def analyse_segments(df:pd.DataFrame, value_col: str, segments: list):
 
         # Calculate absolute and relative change from first point to last point of trend.
         # (Using min/max instead of first/last to be more robust to noise.)
-        if segment['direction'] == 'Up': # max - min
-            segment_enhanced['change'] = float(df_segment[value_col].max() - df_segment[value_col].min())
-            segment_enhanced['pct_change'] = float(df_segment[value_col].max()/df_segment[value_col].min() -1)
-        if segment['direction'] == 'Down': # min - max
-            segment_enhanced['change'] = float(df_segment[value_col].min() - df_segment[value_col].max())
-            segment_enhanced['pct_change'] = float(df_segment[value_col].min()/df_segment[value_col].max() -1)
+        val_min = df_segment[value_col].min()
+        val_max = df_segment[value_col].max()
+        if segment['direction'] == 'Up':  # max - min
+            segment_enhanced['change'] = float(val_max - val_min)
+            segment_enhanced['pct_change'] = (
+                float(val_max / val_min - 1) if val_min != 0 else np.nan
+            )
+        elif segment['direction'] == 'Down':  # min - max
+            segment_enhanced['change'] = float(val_min - val_max)
+            segment_enhanced['pct_change'] = (
+                float(val_min / val_max - 1) if val_max != 0 else np.nan
+            )
 
         # Calculate days & cumulative total change
         segment_enhanced['days'] = (pd.to_datetime(segment['end']) - pd.to_datetime(segment['start'])).days
@@ -25,8 +31,7 @@ def analyse_segments(df:pd.DataFrame, value_col: str, segments: list):
         # Calculate Signal to Noise Ratio
         signal_power = np.mean(df_segment['signal']**2)
         noise_power = np.mean(df_segment['noise']**2)
-        segment_enhanced['SNR'] = float(10 * np.log10(signal_power / noise_power))
-
+        segment_enhanced['SNR'] = float(10 * np.log10(signal_power / noise_power)) if noise_power != 0 else np.nan
         segments_enhanced.append(segment_enhanced)
 
     # Establish time index, earliest to latest
