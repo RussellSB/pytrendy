d050e79264666193db05404e1c1db0075b38688d
Russell SB <r.sammutbonnici@gmail.com>
2025-10-04 20:52:47 +0100

    temp(fix): commenting out whole noise distinguish logic actually improves noise detection. Caters for edge case of it not being catered for well by flat, and making up/down detected to sensitively around it.

--- pytrendy/process_signals.py
+++ pytrendy/process_signals.py
@@ -1,8 +1,9 @@
-import pandas as pd
-from scipy.signal import savgol_filter
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
+from scipy.signal import savgol_filter
+from scipy.signal import butter, filtfilt
 from .io.data_loader import load_data
 from .simpledtw import dtw
 
@@ -12,12 +13,20 @@ def process_signals(df:pd.DataFrame, value_col: str):
     WINDOW_FLAT = int(WINDOW_SMOOTH*0.5)
     WINDOW_NOISE = int(WINDOW_SMOOTH*0.5)
 
-    THRESHOLD_ZEROS = 0.50 # Sensitivity to zeros pct, needed for nosie by snr (0-1)
+    THRESHOLD_ZEROS = 0.50 # Sensitivity to zeros pct, needed for nosie by snr (0-1)asdasdas
     THRESHOLD_NOISE = 5 # Sensitivity to detecting noise (recommended 0-10)
     THRESHOLD_SMOOTH = 0.25 # Sensetivity to detecting trends (recommended 0-0.5)
 
+    # # Define filter parameters
+    # ORDER = 3
+    # CUTOFF = 0.05
+    # b, a = butter(ORDER, CUTOFF, btype='low', analog=False)
+    # df['smoothed'] = filtfilt(b, a, df[value_col])
+
+    print('CHANGE 5')
+
     # 1. Savgol filter (rolling avg improvement). Caters for seasonality with tightness to day.
-    df['smoothed'] = savgol_filter(df[value_col], window_length=WINDOW_SMOOTH, polyorder=1)
+    df['smoothed'] = savgol_filter(df[value_col], window_length=WINDOW_FLAT, polyorder=1)
 
     # 2. Flat detection using rolling std of savgol filter.
     # with leading and trailing to cater for periods centered windows doesnt cover
@@ -28,6 +37,7 @@ def process_signals(df:pd.DataFrame, value_col: str):
     df['flat_flag'] = 0
     rolling_std = df[value_col].rolling(WINDOW_FLAT, center=True).std()
     min_nonzero_std = rolling_std[rolling_std > 0].min()
+    print(min_nonzero_std)
     df.loc[df['smoothed_std'] <= min_nonzero_std, 'flat_flag'] = 1 # can comment out to not care about flats. Just take flats with up/down
 
     # 3. Noise detection via SNR. 
@@ -47,72 +57,98 @@ def process_signals(df:pd.DataFrame, value_col: str):
     df.loc[(df['snr'] <= THRESHOLD_NOISE) & (df['zeros_pct'] <= THRESHOLD_ZEROS), 'noise_flag'] = 1
 
     # 3.4 Double check & refresh noise flag. Distinguish noise from abrupt change.
-    df['noise_flag_diff'] = df['noise_flag'].diff()
-    noise_starts = df.loc[df['noise_flag_diff'] == 1].index
-    noise_ends = df.loc[df['noise_flag_diff'] == -1].index
+    # df['noise_flag_diff'] = df['noise_flag'].diff()
+    # noise_starts = df.loc[df['noise_flag_diff'] == 1].index
+    # noise_ends = df.loc[df['noise_flag_diff'] == -1].index
     
     # Construct noise segments list based on flag_diff
-    noise_segments = []
-    for noise_start in noise_starts: # Loops from first start onwards
-        after_ends = [end for end in noise_ends if end > noise_start]
-        if len(after_ends) > 0:
-            noise_end = after_ends[0]
-        else:
-            noise_end = min(noise_start + pd.Timedelta(days=1), df.index[-1])
-        noise_segments.append(dict(start=noise_start, end=noise_end))
+    # noise_segments = []
+    # for noise_start in noise_starts: # Loops from first start onwards
+    #     after_ends = [end for end in noise_ends if end > noise_start]
+    #     if len(after_ends) > 0:
+    #         noise_end = after_ends[0]
+    #     else:
+    #         noise_end = min(noise_start + pd.Timedelta(days=1), df.index[-1])
+    #     noise_segments.append(dict(start=noise_start, end=noise_end))
 
-    if len(noise_ends) > 0: # Adds noise end with no start if at beginning
-        noise_end = noise_ends[0]
-        early_starts = [start for start in noise_starts if start < noise_end]
-        if len(early_starts) == 0:
-            noise_start = max(noise_end - pd.Timedelta(days=1), df.index[0])
-            noise_segments.insert(0, dict(start=noise_start, end=noise_end))
+    # if len(noise_ends) > 0: # Adds noise end with no start if at beginning
+    #     noise_end = noise_ends[0]
+    #     early_starts = [start for start in noise_starts if start < noise_end]
+    #     if len(early_starts) == 0:
+    #         noise_start = max(noise_end - pd.Timedelta(days=1), df.index[0])
+    #         noise_segments.insert(0, dict(start=noise_start, end=noise_end))
 
-    # Loads classes signals
-    if len(noise_segments) > 0: 
-        df_class = load_data('classes_signals')
-        df_class.set_index('date', inplace=True)
-        df_class = (df_class - df_class.min()) / (df_class.max() - df_class.min())
+    # # Loads classes signals
+    # if len(noise_segments) > 0: 
+    #     df_class = load_data('classes_signals')
+    #     df_class.set_index('date', inplace=True)
+    #     df_class = (df_class - df_class.min()) / (df_class.max() - df_class.min())
 
     # Distinguishes noise signals from abrupt change trends, sets noise flag to 0 when overlaps abrupt.
-    def is_noise_signal(df, start, end):
-        """Checks if noise signal using DTW cost function."""
-        df_segment = df.loc[start:end]
-        df_segment = (df_segment - df_segment.min()) / (df_segment.max() - df_segment.min())
+    # def is_noise_signal(df, start, end):
+    #     """Checks if noise signal using DTW cost function."""
+    #     df_segment = df.loc[start:end]
+    #     df_segment = (df_segment - df_segment.min()) / (df_segment.max() - df_segment.min())
 
-        _, cost_abrupt_up, _, _, _ = dtw(df_segment[value_col], df_class['abrupt_up'])
-        _, cost_abrupt_down, _, _, _ = dtw(df_segment[value_col], df_class['abrupt_down'])
-        _, cost_noise_up, _, _, _ = dtw(df_segment[value_col], df_class['noise_up'])
-        _, cost_noise_down, _, _, _ = dtw(df_segment[value_col], df_class['noise_down'])
+    #     _, cost_abrupt_up, _, _, _ = dtw(df_segment[value_col], df_class['abrupt_up'])
+    #     _, cost_abrupt_down, _, _, _ = dtw(df_segment[value_col], df_class['abrupt_down'])
+    #     _, cost_noise_up, _, _, _ = dtw(df_segment[value_col], df_class['noise_up'])
+    #     _, cost_noise_down, _, _, _ = dtw(df_segment[value_col], df_class['noise_down'])
 
-        if np.argmin([cost_noise_up, cost_noise_down, cost_abrupt_up, cost_abrupt_down]) < 2:
-            return True
-        else: 
-            return False
+    #     if np.argmin([cost_noise_up, cost_noise_down, cost_abrupt_up, cost_abrupt_down]) < 2:
+    #         return True
+    #     else: 
+    #         return False
         
-    for segment in noise_segments:
+    # for segment in noise_segments:
 
-        # Pass 1: Check if immediate noise segment matches
-        start = segment['start'] 
-        end = segment['end'] 
-        if is_noise_signal(df, start, end):
-            df.loc[start:end, 'noise_flag'] = 0
-            continue
+    #     # Pass 1: Check if immediate noise segment matches
+    #     start = segment['start'] 
+    #     end = segment['end'] 
+    #     if is_noise_signal(df, start, end):
+    #         df.loc[start:end, 'noise_flag'] = 0
+    #         continue
 
-        # Pass 2: If it doesn't, check once more with more leniency.
-        width = (segment['end'] - segment['start']).days
-        start_padded = segment['start'] - pd.Timedelta(days=width)
-        end_padded = segment['end'] + pd.Timedelta(days=width)
-        if is_noise_signal(df, start_padded, end_padded):
-            df.loc[start:end, 'noise_flag'] = 0
+    #     # Pass 2: If it doesn't, check once more with more leniency.
+    #     width = (segment['end'] - segment['start']).days
+    #     start_padded = segment['start'] - pd.Timedelta(days=width)
+    #     end_padded = segment['end'] + pd.Timedelta(days=width)
+    #     if is_noise_signal(df, start_padded, end_padded):
+    #         df.loc[start:end, 'noise_flag'] = 0
 
     # 4. Detect up/down trend. Uses first derivates of savgol filter (like diff). 
-    # Results in signal that's uptrend > 0, else down. As long as its not on a flat.
+    # Savgol filter (rolling avg improvement). Caters for seasonality with tightness to day.
+    # Results in signal that's uptrend > 0, else down. As long as its not on a flat or noise.
     df['trend_flag'] = 0
     df.loc[df['flat_flag'] == 1, 'trend_flag'] = -2
     df.loc[df['noise_flag'] == 1, 'trend_flag'] = -3
     df['smoothed_deriv'] = savgol_filter(df[value_col], window_length=WINDOW_SMOOTH, polyorder=1, deriv=1)
     df.loc[(df['smoothed_deriv'] >= THRESHOLD_SMOOTH) & (df['flat_flag'] == 0) & (df['noise_flag'] == 0), 'trend_flag'] = 1
     df.loc[(df['smoothed_deriv'] < -THRESHOLD_SMOOTH) & (df['flat_flag'] == 0) & (df['noise_flag'] == 0), 'trend_flag'] = -1
+
+    
+    ax = df[[value_col, 'smoothed']].plot(figsize=(20,3), secondary_y='smoothed')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
+
+    ax = df[[value_col, 'smoothed_std']].plot(figsize=(20,3), secondary_y='smoothed_std')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
+
+    ax = df[[value_col, 'flat_flag']].plot(figsize=(20,3), secondary_y='flat_flag')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
+    
+    ax = df[[value_col, 'noise_flag']].plot(figsize=(20,3), secondary_y='noise_flag')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
+
+    ax = df[[value_col, 'smoothed_deriv']].plot(figsize=(20,3), secondary_y='smoothed_deriv')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
+
+    ax = df[[value_col, 'trend_flag']].plot(figsize=(20,3), secondary_y='trend_flag')
+    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    plt.show()
 
     return df
--- tests/test.py
+++ tests/test.py
@@ -9,9 +9,11 @@ os.getcwd()
 # %%
 %reload_ext autoreload
 
-#%%
+# %%
 import pytrendy as pt
-# Example Runs
+
+#%%
+# synth 1
 df = pt.load_data('series_synthetic')
 df.set_index('date', inplace=True)
 df.loc['2025-01-01':'2025-02-11', 'abrupt'] = 0
@@ -19,16 +21,51 @@ df.loc['2025-02-16':'2025-03-10', 'abrupt'] = 125 # added
 df.loc['2025-03-18':'2025-04-15', 'abrupt'] = 150
 df.loc['2025-03-20':'2025-04-22', 'abrupt'] = 250 # added more recently
 df.loc['2025-03-25':'2025-04-01', 'abrupt'] = 200
-df.loc['2025-05-27':'2025-05-28', 'abrupt'] = 200 # TODO: fix last edge case
+# df[['abrupt']].plot(figsize=(20,5))
+results = pt.detect_trends(df.reset_index(), date_col='date', value_col='abrupt', method_params=dict(is_abrupt_padded=False))
+
+#%%
+# synth 2
+df = pt.load_data('series_synthetic')
+df.set_index('date', inplace=True)
+df.loc['2025-01-01':'2025-02-11', 'abrupt'] = 0
+df.loc['2025-02-16':'2025-03-10', 'abrupt'] = 125 # added
+df.loc['2025-03-18':'2025-04-15', 'abrupt'] = 150
+df.loc['2025-03-20':'2025-04-22', 'abrupt'] = 250 # added more recently
+df.loc['2025-03-25':'2025-04-01', 'abrupt'] = 200
+df.loc['2025-06-01':'2025-06-01', 'abrupt'] = 300 # TODO: fix last edge case
 # df[['abrupt']].plot(figsize=(20,5))
 results = pt.detect_trends(df.reset_index(), date_col='date', value_col='abrupt')
 
 #%%
+# original test cases
 df = pt.load_data('series_synthetic')
+df.set_index('date', inplace=True)
+# df.loc['2025-03-25':'2025-03-25', 'gradual'] = 400 # TODO: fix noise edge case
+df = df.reset_index()
 results_gradual = pt.detect_trends(df, date_col='date', value_col='gradual', plot=True, method_params=dict(is_abrupt_padded=True))
-results_abrupt = pt.detect_trends(df, date_col='date', value_col='abrupt', plot=True, method_params=dict(is_abrupt_padded=True))
+# results_abrupt = pt.detect_trends(df, date_col='date', value_col='abrupt', plot=True, method_params=dict(is_abrupt_padded=True))
 
 # %%
+# noise test 1 - increasing noise 
+import numpy as np
+for noise_std in [0, 10, 15, 20,50]:
+    print(f'Noise value: {noise_std}')
+    df = pt.load_data('series_synthetic')
+    df['value_noisy'] = df['gradual'] + np.random.normal(0, noise_std, size=len(df))
+    results = pt.detect_trends(df, date_col='date', value_col='value_noisy')
+
+# %%
+# noise test 2 - add a spike
+df = pt.load_data('series_synthetic')
+df.set_index('date', inplace=True)
+df.loc['2025-03-25':'2025-03-25', 'gradual'] = 400 # TODO: fix noise edge case
+df = df.reset_index()
+results_gradual = pt.detect_trends(df, date_col='date', value_col='gradual', plot=True, method_params=dict(is_abrupt_padded=True))
+# results_abrupt = pt.detect_trends(df, date_col='date', value_col='abrupt', plot=True, method_params=dict(is_abrupt_padded=True))
+
+# %%
+# noise test 3 - run till crashes
 import numpy as np
 for i in range(50):
     for noise_std in [10]: #[0, 10, 15, 20,50]
