802e1e563664de19fb3ef25c0687361b03d48420
Russell SB <r.sammutbonnici@gmail.com>
2025-10-05 18:55:53 +0100

    chore: put flat after noise, to try depend on it for edge case catering. Also removing zero pct flat feature as no longer useful

--- pytrendy/process_signals.py
+++ pytrendy/process_signals.py
@@ -7,41 +7,26 @@ def process_signals(df:pd.DataFrame, value_col: str):
     WINDOW_SMOOTH = 15
     WINDOW_FLAT = int(WINDOW_SMOOTH*0.5)
     WINDOW_NOISE = int(WINDOW_SMOOTH*0.5)
-
-    THRESHOLD_ZEROS = 0.50 # Sensitivity to zeros pct, needed for nosie by snr (0-1)asdasdas
+    
     THRESHOLD_NOISE = 5 # Sensitivity to detecting noise (recommended 0-10)
     THRESHOLD_SMOOTH = 0.25 # Sensetivity to detecting trends (recommended 0-0.5)
 
-    # 1. Flat detection using rolling std of savgol filter.
-    # with leading and trailing to cater for periods centered windows doesnt cover
-    df['smoothed'] = savgol_filter(df[value_col], window_length=WINDOW_SMOOTH, polyorder=1)
-
-    df['smoothed_std'] = df['smoothed'].rolling(WINDOW_FLAT, center=True).std()
-    df['smoothed_std_leading'] = df['smoothed'].iloc[::-1].rolling(window=WINDOW_FLAT).std().iloc[::-1]
-    df['smoothed_std_trailing'] = df['smoothed'].rolling(WINDOW_FLAT).std()
-    df['smoothed_std'] = df['smoothed_std'].fillna(df['smoothed_std_leading']).fillna(df['smoothed_std_trailing'])
-
-    df['flat_flag'] = 0
-    rolling_std = df[value_col].rolling(WINDOW_FLAT, center=True).std()
-    min_nonzero_std = rolling_std[rolling_std > 0].min()
-    df.loc[df['smoothed_std'] <= min_nonzero_std, 'flat_flag'] = 1 # can comment out to not care about flats. Just take flats with up/down
-
-    # 2. Noise detection via SNR. 
-    # 2.1 Compute zero flat edge cases
+    # 1. Noise detection via SNR. 
+    # 1.1 Compute zero flat edge cases
     df['zeros_pct_trailing'] = (df[value_col] == 0).rolling(WINDOW_NOISE, min_periods=1).sum() / WINDOW_NOISE
     df['zeros_pct_leading'] = ((df[value_col] == 0).iloc[::-1].rolling(window=WINDOW_NOISE, min_periods=1).sum() / WINDOW_NOISE).iloc[::-1]
     df['zeros_pct'] = df[['zeros_pct_trailing', 'zeros_pct_leading']].max(axis=1)
 
-    # 2.2 Compute the SNR
+    # 1.2 Compute the SNR
     df['signal'] = df[value_col].rolling(window=WINDOW_NOISE, center=True, min_periods=1).mean()
     df['noise'] = df[value_col] - df['signal']
     df['snr'] = 10 * np.log10(df['signal']**2 / df['noise']**2)
 
-    # 2.3 Define noise flag when SNR & not all zero
+    # 1.3 Define noise flag when SNR & not all zero
     df['noise_flag'] = 0
-    df.loc[(df['snr'] <= THRESHOLD_NOISE)  & (df['zeros_pct'] <= THRESHOLD_ZEROS), 'noise_flag'] = 1 
+    df.loc[(df['snr'] <= THRESHOLD_NOISE), 'noise_flag'] = 1 #   & (df['zeros_pct'] <= THRESHOLD_ZEROS)
     
-    # 3.4 Double check & refresh noise flag. Distinguish noise from abrupt change.
+    # 1.4 Double check & refresh noise flag. Distinguish noise from abrupt change.
     df['noise_flag_diff'] = df['noise_flag'].diff()
     noise_starts = df.loc[df['noise_flag_diff'] == 1].index
     noise_ends = df.loc[df['noise_flag_diff'] == -1].index
@@ -78,6 +63,20 @@ def process_signals(df:pd.DataFrame, value_col: str):
         elif (pd.to_datetime(segment['end']) - pd.to_datetime(segment['start'])).days == 1:
             df.loc[start:end, 'noise_flag'] = 1 # caters for edge case of noise outlier, stretch it out for visibility
 
+    # 2. Flat detection using rolling std of savgol filter.
+    # with leading and trailing to cater for periods centered windows doesnt cover
+    df['smoothed'] = savgol_filter(df[value_col], window_length=WINDOW_SMOOTH, polyorder=1)
+
+    df['smoothed_std'] = df['smoothed'].rolling(WINDOW_FLAT, center=True).std()
+    df['smoothed_std_leading'] = df['smoothed'].iloc[::-1].rolling(window=WINDOW_FLAT).std().iloc[::-1]
+    df['smoothed_std_trailing'] = df['smoothed'].rolling(WINDOW_FLAT).std()
+    df['smoothed_std'] = df['smoothed_std'].fillna(df['smoothed_std_leading']).fillna(df['smoothed_std_trailing'])
+
+    df['flat_flag'] = 0
+    rolling_std = df[value_col].rolling(WINDOW_FLAT, center=True).std()
+    min_nonzero_std = rolling_std[rolling_std > 0].min()
+    df.loc[df['smoothed_std'] <= min_nonzero_std, 'flat_flag'] = 1 # can comment out to not care about flats. Just take flats with up/down
+
     # 3. Detect up/down trend. Uses first derivates of savgol filter (like diff). 
     # Savgol filter (rolling avg improvement). Caters for seasonality with tightness to day.
     # Results in signal that's uptrend > 0, else down. As long as its not on a flat or noise.
@@ -88,12 +87,11 @@ def process_signals(df:pd.DataFrame, value_col: str):
     df.loc[(df['smoothed_deriv'] >= THRESHOLD_SMOOTH) & (df['flat_flag'] == 0) & (df['noise_flag'] == 0), 'trend_flag'] = 1
     df.loc[(df['smoothed_deriv'] < -THRESHOLD_SMOOTH) & (df['flat_flag'] == 0) & (df['noise_flag'] == 0), 'trend_flag'] = -1
 
-
     import matplotlib.pyplot as plt
 
-    ax = df[[value_col, 'smoothed']].plot(figsize=(20,3), secondary_y='smoothed')
-    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
-    plt.show()
+    # ax = df[[value_col, 'smoothed']].plot(figsize=(20,3), secondary_y='smoothed')
+    # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    # plt.show()
 
     ax = df[[value_col, 'smoothed_std']].plot(figsize=(20,3), secondary_y='smoothed_std')
     ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
@@ -107,12 +105,12 @@ def process_signals(df:pd.DataFrame, value_col: str):
     ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
     plt.show()
 
-    ax = df[[value_col, 'smoothed_deriv']].plot(figsize=(20,3), secondary_y='smoothed_deriv')
-    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
-    plt.show()
+    # ax = df[[value_col, 'smoothed_deriv']].plot(figsize=(20,3), secondary_y='smoothed_deriv')
+    # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    # plt.show()
 
-    ax = df[[value_col, 'trend_flag']].plot(figsize=(20,3), secondary_y='trend_flag')
-    ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
-    plt.show()
+    # ax = df[[value_col, 'trend_flag']].plot(figsize=(20,3), secondary_y='trend_flag')
+    # ax.right_ax.axhline(y=0, color='gray', linestyle='--', linewidth=2)
+    # plt.show()
 
     return df
