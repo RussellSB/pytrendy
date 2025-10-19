683f99cc6859c71e93ce7b502b7a0925356cc48e
Russell SB <r.sammutbonnici@gmail.com>
2025-10-19 09:58:22 +0100

    fix(clean artifacts): Removing clean when trend too flat. Should not be mis-detected on flat anymore now that signal processing is based on clean signal.

--- tests/test.py
+++ tests/test.py
@@ -20,7 +20,7 @@ import pandas as pd
 df = pt.load_data('series_synthetic')
 df.set_index('date', inplace=True)
 df.loc['2025-01-01':'2025-02-11', 'abrupt'] = 0
-df.loc['2025-02-16':'2025-03-10', 'abrupt'] = 125
+df.loc['2025-02-16':'2025-03-10', 'abrupt'] = 125 # TODONE: cater for clean artifacts update, dont clean if too flat
 df.loc['2025-03-18':'2025-04-15', 'abrupt'] = 150
 df.loc['2025-03-20':'2025-04-22', 'abrupt'] = 250
 df.loc['2025-03-25':'2025-04-01', 'abrupt'] = 200 
@@ -126,7 +126,7 @@ for noise_std in [10]*10: #[0, 10, 15, 20,50]
 df = pt.load_data('series_synthetic')
 df.set_index('date', inplace=True)
 df.loc['2025-03-25':'2025-03-25', 'gradual'] = 200  # TODONE: improve that it doesnt cover full one noise spike. #TODONE: improve that bad red stretches good green change rank 2
-# TODO (in progress): fix that neglects downtrend start, on left of noise
+# TODONE: fix that neglects downtrend start, on left of noise
 df = df.reset_index()
 results_gradual = pt.detect_trends(df, date_col='date', value_col='gradual', plot=True, method_params=dict(is_abrupt_padded=True))
 
