f16390085f4dbb6b82724b66b1717133c6484c7f
Russell SB <r.sammutbonnici@gmail.com>
2025-10-26 08:43:08 +0000

    fix(plot): improving plotting for edge cases found from noise plotting. Make it visually displace appropriately only when it doesn't break up/down logic

--- pytrendy/io/plot_pytrendy.py
+++ pytrendy/io/plot_pytrendy.py
@@ -45,19 +45,65 @@ def plot_pytrendy(df:pd.DataFrame, value_col: str, segments_enhanced:list):
         end = pd.to_datetime(seg['end'])
         color = color_map.get(seg['direction'], 'gray')
 
-        if ('trend_class' in seg and seg['trend_class'] == 'abrupt') or seg['direction'] == 'Noise': 
-            start = start # Conditional logic for making abrupt visually tighter
-        else: start = start - pd.Timedelta(days=1) # Everything else displaced left start
+        # Get context on prev seg if possible
+        prev_seg = segments_enhanced[i-1] if i-1 >= 0 else None
+        prev_neighbouring = prev_seg and (pd.to_datetime(prev_seg['end']) == (start - pd.Timedelta(days=1)))
+        is_prev_not_trend = prev_seg and (not ('trend_class' in prev_seg))
+
+        # Current seg context
+        is_abrupt = ('trend_class' in seg and seg['trend_class'] == 'abrupt')
+        is_noise = (seg['direction'] == 'Noise')
+        is_not_trend = not ('trend_class' in seg)
 
         # Get context on next seg if possible
         next_seg = segments_enhanced[i+1] if i+1 < len(segments_enhanced) else None
-        neighbouring = next_seg and (pd.to_datetime(next_seg['start']) == (end + pd.Timedelta(days=1)))
+        next_neighbouring = next_seg and (pd.to_datetime(next_seg['start']) == (end + pd.Timedelta(days=1)))
+        next_seg_abrupt = next_seg and (('trend_class' in next_seg) and (next_seg['trend_class'] == 'abrupt'))
+        next_seg_noise = next_seg and (next_seg['direction'] == 'Noise')
 
-        # Adjust neighbouring segment before abrupt or noise (visually). Avoid white lines
-        next_seg_abrupt = next_seg and ((('trend_class' in next_seg) and (next_seg['trend_class'] == 'abrupt')) or next_seg['direction'] == 'Noise')
-        if next_seg_abrupt and neighbouring:
-            end = end + pd.Timedelta(days=1)
-        else: end = end
+        # Adjust starts when appropriate
+        if is_abrupt or is_noise: 
+            start = start # Conditional logic for making abrupt visually tighter
+        else: 
+            new_start = start - pd.Timedelta(days=1) # Everything else displaced left start
+
+            # Check validity of plot start adjustment
+            value_new_start = df.loc[new_start, value_col] if new_start in df.index else None
+            value = df.loc[start, value_col]
+
+            valid_up_start = (value_new_start) and (seg['direction'] == 'Up') and (value_new_start < value)
+            valid_down_start = (value_new_start) and (seg['direction'] == 'Down') and (value_new_start > value)
+            if valid_up_start or valid_down_start or is_not_trend:
+                start = new_start # Apply left displacement only if valid
+            else: 
+                # if not displaced and prev is not trend, adjust by plotting (as prev has already been drawn)
+                if is_prev_not_trend and prev_neighbouring: 
+                    prev_end = pd.to_datetime(segments_enhanced[i-1]['end'])
+                    prev_new_end = (prev_end + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+                    mask = (df.index >= prev_end) & (df.index <= prev_new_end) 
+                    prev_color = color_map.get(segments_enhanced[i-1]['direction'], 'gray')
+                    ax.fill_between(df.index[mask], ymin, ymax, color=prev_color, alpha=0.4)
+
+
+        # Adjust ends when appropriate
+        if (next_seg_abrupt or next_seg_noise) and next_neighbouring:
+            new_end = end + pd.Timedelta(days=1)
+            
+            # Check validity of plot end adjustment
+            value_new_end = df.loc[new_end, value_col] if new_end in df.index else None
+            value = df.loc[end, value_col]
+
+            valid_up_end = (value_new_end) and (seg['direction'] == 'Up') and (value_new_end > value)
+            valid_down_end = (value_new_end) and (seg['direction'] == 'Down') and (value_new_end < value)
+            is_not_trend = not ('trend_class' in seg)
+            if valid_up_end or valid_down_end or is_not_trend:
+                end = new_end  # Apply right displacement only if valid
+            else: 
+                # if not displaced and next is noise, adjust for next plotting round
+                if next_seg_noise and next_neighbouring: 
+                    segments_enhanced[i+1]['start'] = (pd.to_datetime(segments_enhanced[i+1]['start']) - pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+        else: 
+            end = end
 
         mask = (df.index >= start) & (df.index <= end) 
         ax.fill_between(df.index[mask], ymin, ymax, color=color, alpha=0.4)
@@ -71,7 +117,7 @@ def plot_pytrendy(df:pd.DataFrame, value_col: str, segments_enhanced:list):
                     color=color[5:])
             
         # Add vertical line if next seg is same & touching
-        if next_seg and neighbouring and next_seg['direction'] == seg['direction']:
+        if next_seg and next_neighbouring and next_seg['direction'] == seg['direction']:
             line_date = pd.to_datetime(seg['end'])
             ax.axvline(x=line_date, color=color[5:], linewidth=0.5)
 
